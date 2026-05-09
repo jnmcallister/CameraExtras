823689ae749362df21e174582fbffabf53d60979
Joshua McAllister <joshnmcallister@gmail.com>
2026-05-09 12:34:39 -0700

    Add camera freezing

--- CameraExtrasPlugin.cs
+++ CameraExtrasPlugin.cs
@@ -8,12 +8,22 @@ namespace CameraExtras;
 [BepInAutoPlugin(id: "io.github.foxyrobo.cameraextras")]
 public partial class CameraExtrasPlugin : BaseUnityPlugin
 {
+    bool cameraFrozen = false;
+
+    CameraController cameraController;
+
     private void Awake()
     {
         // Put your initialization logic here
         Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
 
+        // Initialize classes
         Configuration.Init(Config);
+    }
+
+    void Start()
+    {
+        cameraController = GameObject.FindFirstObjectByType<CameraController>();
     }
 
     void Update()
@@ -27,6 +37,31 @@ public partial class CameraExtrasPlugin : BaseUnityPlugin
         if (InputHelper.FreezeCameraDown)
         {
             Logger.LogInfo("Freeze camera button pressed");
+            ToggleFreezeCamera();
         }
+    }
+
+    public void ToggleFreezeCamera()
+    {
+        // Make sure camera controller exists
+        if (cameraController == null)
+        {
+            Logger.LogWarning("No camera controller found! Trying again");
+
+            // If none was found, try to find it again
+            cameraController = GameObject.FindFirstObjectByType<CameraController>();
+            if (cameraController == null)
+            {
+                Logger.LogError("No camera controller found!");
+                return;
+            }
+        }
+
+        // Toggle freezing/unfreezing camera controller
+        if (cameraFrozen)
+            cameraController.FreezeInPlace();
+        else
+            cameraController.StopFreeze();
+        cameraFrozen = !cameraFrozen;
     }
 }
